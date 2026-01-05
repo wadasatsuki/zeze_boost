54cb0175747f91511d68791e6feb22e6e438d02b
satsuki.wada <www.satsuki.wada@gmail.com>
2026-01-05 16:28:04 +0900

    remove misplaced health route

--- app/api/health/route.ts
+++
@@ -1,8 +0,0 @@
-import { NextResponse } from "next/server";
-
-export async function GET() {
-  return NextResponse.json({
-    ADMIN_ID: !!process.env.ADMIN_ID,
-    ADMIN_PASSWORD: !!process.env.ADMIN_PASSWORD,
-  });
-}
